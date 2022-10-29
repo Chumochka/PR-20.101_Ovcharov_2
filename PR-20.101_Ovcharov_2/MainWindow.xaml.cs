10d3fef58e024e36119734114f1babf4afee7090
Chumochka <danpro1010@bk.ru>
2022-10-29 12:15:04 +0700

    1

--- PR-20.101_Ovcharov_2/MainWindow.xaml.cs
+++ PR-20.101_Ovcharov_2/MainWindow.xaml.cs
@@ -28,43 +28,51 @@ namespace PR_20._101_Ovcharov_2
         private void btn_work_Click(object sender, RoutedEventArgs e)
         {
             lb_result.Items.Clear();
-            string[] words = tb_words.Text.ToLower().Split(new[] { " " }, System.StringSplitOptions.RemoveEmptyEntries);
-            char[] glas = { 'a', 'e', 'i', 'o', 'u', 'y' };
-            char[] notglas = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z','1','2','3','4','5','6','7','8','9','0','\u0027'};
-            int glas_count = 0;
-            string max_word = words[0];
-            int max_length = words[0].Length;
-            for (int i = 0; i < words.Length; i++)
+            try
             {
-                char[] sym = words[i].ToCharArray();
-                if (words[i].Length >max_length)
+                string[] words = tb_words.Text.ToLower().Split(new[] { " " }, System.StringSplitOptions.RemoveEmptyEntries);
+                char[] glas = { 'a', 'e', 'i', 'o', 'u', 'y' };
+                char[] notglas = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\u0027', '.', ',', '!', '?' };
+                int glas_count = 0;
+                string max_word = words[0];
+                int max_length = words[0].Length;
+                for (int i = 0; i < words.Length; i++)
                 {
-                    max_length = words[i].Length;
-                    max_word = words[i];
-                }
-                for(int j = 0; j < sym.Length; j++)
-                {
-                    if (Array.Exists(glas, element => element ==sym[j]))
+                    char[] sym = words[i].ToCharArray();
+                    if (words[i].Length > max_length)
                     {
-                        glas_count++;
-                    }else if(Array.Exists(notglas, element => element == sym[j]))
-                    {
-
+                        max_length = words[i].Length;
+                        max_word = words[i];
                     }
-                    else
+                    for (int j = 0; j < sym.Length; j++)
                     {
-                        lb_result.Items.Add("Введите предложение состоящие из английских букв");
+                        if (Array.Exists(glas, element => element == sym[j]))
+                        {
+                            glas_count++;
+                        }
+                        else if (Array.Exists(notglas, element => element == sym[j]))
+                        {
+
+                        }
+                        else
+                        {
+                            lb_result.Items.Add("Введите предложение состоящие из английских букв");
+                            break;
+                        }
+                    }
+                    if (lb_result.Items.Count > 0)
+                    {
                         break;
                     }
                 }
-                if (lb_result.Items.Count > 0)
+                if (lb_result.Items.Count == 0)
                 {
-                    break;
+                    lb_result.Items.Add("Количество гласных: " + glas_count + ". Самое длинное слово - " + max_word);
                 }
             }
-            if (lb_result.Items.Count == 0)
+            catch(Exception)
             {
-                lb_result.Items.Add("Количество гласных: " + glas_count + ". Самое длинное слово - " + max_word);
+                lb_result.Items.Add("Введите предложение на английском языке");
             }
         }
     }
